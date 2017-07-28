ee43e4a1c2236d827f3f0f1c471cb79f1aea7090
Christian Kaisermann <christian@kaisermann.me>
2017-07-28 14:01:26 -0300

    Add patspec and autopep8

--- scripts/post.sh
+++ scripts/post.sh
@@ -4,7 +4,7 @@ wp package install aaemnnosttv/wp-cli-dotenv-command
 
 printf "\n${GREEN}>> Configuring python${NC}\n"
 easy_install pip
-pip install fabric Jinja2
+pip install fabric Jinja2 pathspec autopep8 isort
 
 printf "\n${GREEN}>> Configuring PHPCBF Standards${NC}\n"
 phpcs --config-set installed_paths ~/.config/phpcbf/wpcs
