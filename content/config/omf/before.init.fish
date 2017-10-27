f97d20b69ba067777cd217301512c6ae3e6b2a05
Christian Kaisermann <christian@kaisermann.me>
2017-10-27 19:33:13 -0200

    Remove the fuck and non-used paths

--- content/config/omf/before.init.fish
+++ content/config/omf/before.init.fish
@@ -5,7 +5,7 @@ for p in /opt/bin /opt/local/bin ~/.config/fish/bin /usr/bin /usr/local/bin
 	end
 end
 # Adds yarn's default global packages 'bin' path
-command -v yarn > /dev/null; and set -x PATH (yarn global bin) $PATH
+#command -v yarn > /dev/null; and set -x PATH (yarn global bin) $PATH
 
 # Adds coreutils path
 [ -d /usr/local/opt/coreutils/libexec/gnubin ]; and set -x PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
