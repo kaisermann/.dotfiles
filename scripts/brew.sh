f4fee25485de30a58d67b62734f3820c2fe80289
Christian Kaisermann <christian@kaisermann.me>
2017-11-07 18:53:41 -0200

    Update brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -14,7 +14,7 @@ apps=(
   csshx
   dockutil
   exiftool
-  ffmpeg
+  ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-libass --with-libquvi --with-libvorbis --with-libvpx --with-opus --with-x265
   fish
   gettext
   git
