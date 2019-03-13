501788f112cee8e092179c157522c086a69e4c40
Christian Kaisermann <christian@kaisermann.me>
2019-03-13 13:36:18 -0300

    Move til prompt parts to functions

--- content/config/fish/conf.d/til.fish
+++ content/config/fish/conf.d/til.fish
@@ -1,8 +1,11 @@
+# Make 'dot' the floating point separator
+set -x LC_NUMERIC "en_US.UTF-8"
+
 # Remove annoying fish greeting message
 function fish_greeting
 end
 
-function fish_right_prompt
+function til::git_prompt
     set -g __fish_git_prompt_char_upstream_ahead "↑"
     set -g __fish_git_prompt_char_upstream_behind "↓"
     set -g __fish_git_prompt_char_upstream_prefix ""
@@ -24,24 +27,74 @@ function fish_right_prompt
     set -g __fish_git_prompt_color_invalidstate red
     set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
     set -g __fish_git_prompt_color_cleanstate green --bold
-    set -g __fish_prompt_normal (set_color normal)
 
-    set -l exit_code $status
-    set -l status_color normal
+    echo -sn (__fish_vcs_prompt) " "
+end
+
+function til::time
+    set -l exit_code $argv[1]
+    if test "$CMD_DURATION" -gt 100
+        set_color normal
+
+        if test "$exit_code" -ne 0
+            set_color red
+        end
+
+        printf "%.2fs" (math $CMD_DURATION / 1000 )
+    else
+        set_color green
+        echo -sn (date "+%H:%M:%S")
+    end
+end
+
+function til::whoami
+    if test 0 -eq (id -u $USER) -o ! -z "$SSH_CLIENT"
+        set_color -o white
+        echo -sn (host_info "user@host")
+        set_color normal
+        echo -sn " | "
+    end
+end
+
+function til::dir
+    set -l exit_code $argv[1]
+    set -l pwd_info (pwd_info "/")
+
+    if pwd_is_home
+        set_color -o yellow
+        echo -sn "~/"
+    end
+
+    if test ! -z "$pwd_info[2]"
+        set_color -o yellow
+        echo -sn "$pwd_info[2]/"
+    end
+
+    if test ! -z "$pwd_info[1]"
+        set_color -o white
+        echo -sn "$pwd_info[1]"
+    end
+
+    if test ! -z "$pwd_info[3]"
+        set_color -o yellow
+        echo -sn "/$pwd_info[3]"
+    end
 
     set_color normal
-    printf "%s " (__fish_vcs_prompt)
+end
 
+function til::exit_code
+    set -l exit_code $argv[1]
     if test "$exit_code" -ne 0
         set status_color red
+        echo -sn (set_color red) " $exit_code" (set_color normal)
     end
+end
 
-    if test "$CMD_DURATION" -gt 100
-        set -l duration (echo $CMD_DURATION | humanize_duration)
-        echo -sn (set_color $status_color) "$duration"
-    else
-        echo -sn (set_color green) (date "+%H:%M:%S")
-    end
+function fish_right_prompt
+    set -l exit_code $status
+    til::git_prompt
+    til::time $exit_code
 
     set_color normal
 end
@@ -49,37 +102,17 @@ end
 function fish_prompt
     set -l exit_code $status
     set -l status_color cyan
-    set -l root_glyph "/"
-    set -l pwd_info (pwd_info "/")
-
-    if test 0 -eq (id -u $USER) -o ! -z "$SSH_CLIENT"
-        echo -sn (set_color $status_color) "who(" (set_color normal)
-        echo -sn (set_color -o white) (host_info "user@host") (set_color normal)
-        echo -sn (set_color $status_color) ") " (set_color normal)
-    end
-
-    if pwd_is_home
-        echo -sn (set_color -o yellow) "~/" (set_color normal)
-    end
-
-    if test ! -z "$pwd_info[2]"
-        echo -sn (set_color -o yellow) "$pwd_info[2]/" (set_color normal)
-    end
-
-    if test ! -z "$pwd_info[1]"
-        echo -sn (set_color -o white) "$pwd_info[1]" (set_color normal)
-    end
-
-    if test ! -z "$pwd_info[3]"
-        echo -sn (set_color -o yellow) "/$pwd_info[3]" (set_color normal)
-    end
 
     if test "$exit_code" -ne 0
         set status_color red
-        echo -sn (set_color red) " $exit_code" (set_color normal)
     end
 
+    til::whoami
+    til::dir
+    til::exit_code $exit_code
+
     set_color $status_color
-    printf " \$ "
+    echo -en " \$ "
+
     set_color normal
 end
--- content/config/fish/config.fish
+++ content/config/fish/config.fish
@@ -1,14 +1,26 @@
 # Shell configuration
 set -x fish_greeting ''
-set -x EDITOR 'vim'
+set -x EDITOR vim
 set -x BROWSER open
 set -gx CLICOLOR 1
+set -gx TERM xterm-256color
+set -x LC_NUMERIC "en_US.UTF-8"
+
+# Colored man pages
+set -x LESS_TERMCAP_mb (printf "\033[01;31m")
+set -x LESS_TERMCAP_md (printf "\033[01;31m")
+set -x LESS_TERMCAP_me (printf "\033[0m")
+set -x LESS_TERMCAP_se (printf "\033[0m")
+set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\033[0m")
+set -x LESS_TERMCAP_us (printf "\033[01;32m")
 
 # Some alias
 alias ls 'ls -GFh --color=auto'
+alias grep 'grep --color=always'
 
-# Dircolors
-eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
+# Dircolors (fish's ls already does it)
+# eval (dircolors -c ~/.dircolors | sed 's/>&\/dev\/null$//')
 
 set DIR (readlink -f (dirname (status -f)); and pwd)
 
