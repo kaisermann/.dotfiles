5661c38fb5c1f847b7c2738e005a2235d903a42a
Christian Kaisermann <christian@kaisermann.me>
2019-05-06 17:44:42 -0300

    feat: 🎸 make commands white

--- content/config/fish/config.fish
+++ content/config/fish/config.fish
@@ -6,6 +6,8 @@ set -x BROWSER open
 set -gx CLICOLOR 1
 # set -gx LS_COLORS "di=38;5;27:fi=38;5;7:ln=38;5;51:pi=40;38;5;11:so=38;5;13:or=38;5;197:mi=38;5;161:ex=38;5;9:"
 set -gx TERM xterm-256color
+set -gx fish_color_normal white
+set -gx fish_color_command white
 
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
