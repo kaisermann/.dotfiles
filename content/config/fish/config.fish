3df162418fab37ebd28f1fa607eb72cbeff70831
Christian Kaisermann <christian@kaisermann.me>
2022-02-25 09:22:31 +0100

    Add find_node_bin event handler

--- content/config/fish/config.fish
+++ content/config/fish/config.fish
@@ -1,3 +1,5 @@
+# https://fishshell.com/docs/current/cmds/set.html
+
 # Shell configuration
 set -x fish_greeting ''
 set -x EDITOR vim
@@ -25,13 +27,61 @@ set -x LESS_TERMCAP_us (printf "\033[01;32m")
 set DIR (realpath (dirname (status -f)); and pwd)
 
 source $DIR/abbrs.fish
-source $DIR/functions/node_bin_path.fish
 
+################
+# PATH related #
+################
+
+# https://fishshell.com/docs/current/cmds/fish_add_path.html
+# sets brew path
+fish_add_path -g /opt/homebrew/bin/
+
+# sets volta path
 set -gx VOLTA_HOME "$HOME/.volta"
-
-# sets brew and volta bin paths
-fish_add_path /opt/homebrew/bin/
-fish_add_path $VOLTA_HOME/bin/
+fish_add_path -g $VOLTA_HOME/bin/
 
 # Java runtime
-fish_add_path /opt/homebrew/opt/openjdk@11/bin
+fish_add_path -g /opt/homebrew/opt/openjdk@11/bin
+
+# Remove the given path from the PATH environment variable.
+# Affects current session only
+function remove_from_path
+    if set -l index (contains -i $argv[1] $PATH)
+        set --erase -g fish_user_paths[$index]
+    end
+end
+
+
+function find_node_bin --on-variable PWD
+    set -l current_dir $PWD
+
+    # Remove previous paths from PATH
+    if set -q LOCAL_NODE_BIN_DIR
+        # https://fishshell.com/docs/current/cmds/for.html
+        for path in $LOCAL_NODE_BIN_DIR
+            remove_from_path $path
+        end
+    end
+
+    set -g --erase LOCAL_NODE_BIN_DIR
+
+    # Look for closest node_modules/.bin until hitting /
+    # And collect them on a list
+    while not test (realpath $current_dir) = /
+        set bin_dir $current_dir/node_modules/.bin
+
+        if test -d "$bin_dir"
+            set -gx --append LOCAL_NODE_BIN_DIR $bin_dir
+        end
+
+        set current_dir $current_dir/..
+    end
+
+    # If list is set, add those paths to $PATH
+    if set -q LOCAL_NODE_BIN_DIR
+        fish_add_path -g $LOCAL_NODE_BIN_DIR
+    end
+end
+
+# Run it when a session starts
+find_node_bin
